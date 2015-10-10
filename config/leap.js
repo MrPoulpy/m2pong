f80537a3c8e3820b5c070d9a09ced0905a3b36a7
mickael De PECKER <mickael@cymoz.Fr>
2015-10-10 09:18:55 +0200

    leap.client sails

--- config/leap.js
+++ config/leap.js
@@ -1,3 +1,7 @@
+/**
+ * this need some documentation
+ */
+
 var denis = require('socket.io-client')('http://10.33.32.46:1337');
 var magger = require('socket.io-client')('http://10.33.32.45:1337');
 var rosa = require('socket.io-client')('http://10.33.32.21:1337');
